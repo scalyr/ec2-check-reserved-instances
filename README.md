201eeab5337bfe2f7025d0c4be3fd0c4d2d81921
Christopher Council <chris@scalyr.com>
2016-03-07 20:35:27 -0800

    Added more command-line options, externalize config to files Update README.md

--- README.md
+++ README.md
@@ -1,32 +1,25 @@
-ec2-check-reserved-instances
-============================
+# ec2-check-reserved-instances
 
-EC2 Check Reserved Instances - Compare instance reservations with running instances
+Compares EC2 instance reservations with running instances.
 
-Amazon's reserved instances (ec2-describe-reserved-instances, ec2-describe-reserved-instances-offerings) are a great way to save money when using EC2. An EC2 instance reservation is specified by an availability zone, instance type, and quantity. Correlating the reservations you currently have active with your running instances is a manual, time-consuming, and error prone process.
+Amazon's reserved instances (ec2-describe-reserved-instances, ec2-describe-reserved-instances-offerings) are a great way to save money when using EC2. An EC2 instance reservation is specified by an availability zone, instance type, and quantity. Correlating the reservations you currently have active with your running instances is a manual, time-consuming, and error-prone process.
 
-This quick little Python script uses boto to inspect your reserved instances and running instances to determine if you currently have any reserved instances which are not being used. Additionally, it will give you a list of non-reserved instances which could benefit from additional reserved instance allocations.
+This quick little Python script uses boto3 to inspect your reserved instances and running instances to determine if you currently have any reserved instances which are not being used. Additionally, it will give you a list of non-reserved instances which could benefit from additional reserved instance allocations.
 
-To use the program, make sure you have boto installed. If you don't already have it, run:
+To use the program, make sure you have boto3 installed. If you don't already have it, run:
 
-$ easy_install boto
+`$ easy_install boto3`
 
 or
 
-$ pip install boto
+`$ pip install boto3`
 
 
+### Example Output
 
-The only configuration needed is your AWS keys. These can either be modified at the top of the file, ec2-check-reserved-instances.py, or by exporting your keys in an environment variable:
-
-$ export AWSAccessKeyId=1234567
-
-$ export AWSSecretKey=j3jfijfisa83j+io4jfioajioaw
-
-EXAMPLE OUTPUT
-===============
 ```
-vela:~/dev epheph$ ./ec2-check-reserved-instances.py
+$ ./ec2-check-reserved-instances.py -r us-east-1
+>>> Connecting to EC2...
 UNUSED RESERVATION!	(1)	m1.small	us-east-1b
 UNUSED RESERVATION!	(1)	m2.2xlarge	us-east-1a
 
@@ -39,17 +32,113 @@ Instance not reserved:	(1)	m2.2xlarge	us-east-1b
 (18) reservations
 ```
 
-In this example, you can easily see that an m2.2xlarge was spun up in the wrong AZ (us-east-1b vs. us-east-1a), as well as an m1.small. The "Instance not reserved" section shows that you could benefit from reserving:
-* (1) t1.micro
-* (1) m1.small (not 2, since you'll likely want to move your us-east-1b small to us-east-1d)
-* (3) m1.medium
+
+# Command-line options
+
+## -r or --region (required)
+
+AWS region to check. Example: 
+
+```
+./ec2-check-reserved-instances.py -r us-east-1
+```
+
+## -c or --config_file (default: ~/.s3cfg)
+
+Your AWS credentials should go in a JSON-format configuration file as shown below:
+
+```
+{
+	"aws_access_key_id": "XXXXX",
+	"aws_secret_access_key": "YYYYY"
+}
+```
+
+Example:
+
+```
+./ec2-check-reserved-instances.py -r us-east-1 -c path/to/s3config.json
+```
+
+If not explicitly specified, this script will assume the credentials are stored in `~/.s3cfg`.
 
 
-TODO
-===============
-- Add some sort of sorting, by Availability Zone/instance type
-- Add VPC support
-- Currently only supports US-EAST. Make using other regions easy, perhaps just using an environment variable/internal config variable
-- External config? Is there some standard place to store AWS credentials?
-- Add option to use API to purchase the additional reservations (need to be EXTREMELY careful here, any mistake or miscommunication could cost quite a bit of money)
-- Windows? Not taking Windows reserved instances into account
+## -i or --ignore_file
+
+You may have instances or reservations you don't want to see in the results - maybe you've put some unused reservations up for sale, or maybe you have some micro instances you don't care all that much about. You can specify the types of reservations and/or running instances you want to ignore in another JSON file:
+
+```
+{ 
+	"reserved": [
+		{ "type": "hi1.4xlarge", "zone": "us-east-1b", "count": 1 },
+		{ "type": "hi1.4xlarge", "zone": "us-east-1a", "count": 1 },
+	],
+	"running": [
+		{ "type": "t2.micro", "zone": "us-east-1e", "count": 5 }
+	]
+}
+```
+
+Then, pass the name of that file on the command line:
+
+```
+./ec2-check-reserved-instances.py -r us-east-1 -i ignore.json
+```
+
+This is particularly useful if you're running this script in a cron job and want to send out an alert when unexpected results show up.
+
+
+## -v or --vpc_sensitive (default false)
+
+When you create a new reservation, one of the choices you make is whether it will be a Classic or VPC reservation. This guarantees you resources in the environment specified, which is an important consideration for critical parts of your infrastructure.
+
+However, this distinction is irrelevant if you're primarily concerned about the cost savings that come with reserved instances. This script assumes that you're mainly concerned about cost, so by default, the VPC/Classic distinction is ignored.
+
+If you would like to see VPC/Classic mismatches, specify the -v flag on the command line, as in the example below.
+
+#### Output with -v option
+
+```
+$ ./ec2-check-reserved-instances.py -r us-east-1 -v
+>>> Connecting to EC2...
+UNUSED RESERVATION!	(1)	m3.large	us-east-1b_vpc
+UNUSED RESERVATION!	(1)	m3.medium	us-east-1c_vpc
+UNUSED RESERVATION!	(2)	c3.large	us-east-1a_vpc
+UNUSED RESERVATION!	(1)	m1.small	us-east-1c
+UNUSED RESERVATION!	(1)	i2.xlarge	us-east-1a_vpc
+...
+UNUSED RESERVATION!	(1)	c3.large	us-east-1c_vpc
+UNUSED RESERVATION!	(1)	c3.2xlarge	us-east-1a_vpc
+UNUSED RESERVATION!	(6)	m2.xlarge	us-east-1c_vpc
+UNUSED RESERVATION!	(1)	t2.micro	us-east-1a_vpc
+
+Instance not reserved:	(1)	c3.large	us-east-1b
+Instance not reserved:	(1)	c3.2xlarge	us-east-1b
+Instance not reserved:	(1)	i2.xlarge	us-east-1a
+Instance not reserved:	(1)	c3.large	us-east-1c
+...
+Instance not reserved:	(1)	c3.2xlarge	us-east-1c
+Instance not reserved:	(1)	c3.2xlarge	us-east-1a
+Instance not reserved:	(6)	m2.xlarge	us-east-1c
+Instance not reserved:	(1)	i2.xlarge	us-east-1b
+
+(232) running on-demand instances
+(238) reservations
+```
+
+#### Equivalent output without -v 
+
+```
+$ ./ec2-check-reserved-instances.py -r us-east-1
+>>> Connecting to EC2...
+UNUSED RESERVATION!	(1)	t2.micro	us-east-1a
+UNUSED RESERVATION!	(1)	m3.medium	us-east-1c
+UNUSED RESERVATION!	(2)	t1.micro	us-east-1c
+UNUSED RESERVATION!	(1)	m1.small	us-east-1c
+UNUSED RESERVATION!	(1)	t1.micro	us-east-1a
+
+Congratulations, you have no unreserved instances
+
+(232) running on-demand instances
+(238) reservations
+```
--- ec2-check-reserved-instances.py
+++ ec2-check-reserved-instances.py
@@ -1,80 +1,113 @@
-from __future__ import print_function
-import sys
-import os
-import boto3
-from pprint import pprint
+#!/usr/bin/python
 
-ec2_conn = boto3.client('ec2')
+import argparse
+import json
+from os.path import expanduser
+
+import boto3
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--vpc_sensitive", dest="vpc_sensitive", action='store_true', default=False,
+                    help="Add this flag if you wish to see VPC-related discrepancies")
+parser.add_argument("-r", "--region", dest="region_name", help="AWS region", required=True)
+parser.add_argument("-c", "--config_file", dest="config_file", help="Config file containing credentials",
+                    required=False)
+parser.add_argument("-i", "--ignore_file", dest="ignore_file", help="Config file containing instance types to ignore",
+                    required=False)
+
+options = parser.parse_args()
+
+# load config (JSON file)
+filename = options.config_file if options.config_file else "{0}/.s3cfg".format(expanduser("~"))
+with open(filename) as data_file:
+    data = json.load(data_file)
+
+ignore = None
+if options.ignore_file:
+    with open(options.ignore_file) as data_file:
+        ignore = json.load(data_file)
+
+# connect to ec2
+print(">>> Connecting to EC2...")
+ec2_conn = boto3.client('ec2', region_name=options.region_name,
+                        aws_access_key_id=data['aws_access_key_id'],
+                        aws_secret_access_key=data['aws_secret_access_key'])
 debug = False
 
-def lambda_handler(event, context):
-    reservations = ec2_conn.describe_instances()
-    running_instances = {}
-    for reservation in reservations['Reservations']:
-    	for instance in reservation['Instances']:
-    		if debug:
-        		if instance['State']['Name'] != "running":
-        			print("Disqualifying instance %s: not running" % ( instance['InstanceId'] ) )
-        		elif 'SpotInstanceRequestId' in instance:
-        			print("Disqualifying instance %s: spot" % ( instance['InstanceId'] ) )
-    		if instance['State']['Name'] == "running" and not 'SpotInstanceRequestId' in instance:
-				az = instance['Placement']['AvailabilityZone']
-				if 'VpcId' in instance:
-				    az = az + '_vpc'
-				if 'Platform' in instance:
-				    az = az + '_windows'
-				instance_type = instance['InstanceType']
-				running_instances[ (instance_type, az ) ] = running_instances.get( (instance_type, az ) , 0 ) + 1
-    
-    
-    # pprint( running_instances )
-    
-    
-    reserved_instances = {}
-    reserved_list = ec2_conn.describe_reserved_instances()
-    for reserved_instance in reserved_list['ReservedInstances']:
-    	if debug and reserved_instance['State'] != "active":
-    		print( "Excluding reserved instances %s: no longer active" % ( reserved_instance['ReservedInstancesId'] ) )
-    	if reserved_instance['State'] == "active":
-    		az = reserved_instance['AvailabilityZone']
-    		if 'VPC' in reserved_instance['ProductDescription']:
-    		    az = az + '_vpc'
-    		if 'Windows' in reserved_instance['ProductDescription']:
-    		    az = az + '_windows'
-    		instance_type = reserved_instance['InstanceType']
-    		reserved_instances[( instance_type, az) ] = reserved_instances.get ( (instance_type, az ), 0 )  + reserved_instance['InstanceCount']
-    
-    # pprint( reserved_instances )
-    
-    
-    # this dict will have a positive number if there are unused reservations
-    # and negative number if an instance is on demand
-    instance_diff = dict([(x, reserved_instances[x] - running_instances.get(x, 0 )) for x in reserved_instances])
-    
-    # instance_diff only has the keys that were present in reserved_instances. There's probably a cooler way to add a filtered dict here
-    for placement_key in running_instances:
-    	if not placement_key in reserved_instances:
-    		instance_diff[placement_key] = -running_instances[placement_key]
-    
-    # pprint ( instance_diff )
-    
-    unused_reservations = dict((key,value) for key, value in instance_diff.iteritems() if value > 0)
-    if unused_reservations == {}:
-    	print("Congratulations, you have no unused reservations")
-    else:
-    	for unused_reservation in unused_reservations:
-    		print("UNUSED RESERVATION!\t(%s)\t%s\t%s" % ( unused_reservations[ unused_reservation ], unused_reservation[0], unused_reservation[1] ))
-    
-    print("")
-    
-    unreserved_instances = dict((key,-value) for key, value in instance_diff.iteritems() if value < 0)
-    if unreserved_instances == {}:
-    	print("Congratulations, you have no unreserved instances")
-    else:
-    	for unreserved_instance in unreserved_instances:
-    		print("Instance not reserved:\t(%s)\t%s\t%s" % ( unreserved_instances[ unreserved_instance ], unreserved_instance[0], unreserved_instance[1] ))
-    
-    qty_running_instances = reduce( lambda x, y: x+y, running_instances.values() )
-    qty_reserved_instances = reduce( lambda x, y: x+y, reserved_instances.values() )
-    
-    print("\n(%s) running on-demand instances\n(%s) reservations" % ( qty_running_instances, qty_reserved_instances ))
+reservations = ec2_conn.describe_instances()
+running_instances = {}
+for reservation in reservations['Reservations']:
+    for instance in reservation['Instances']:
+        if debug:
+            if instance['State']['Name'] != "running":
+                print("Disqualifying instance %s: not running" % (instance['InstanceId']))
+            elif 'SpotInstanceRequestId' in instance:
+                print("Disqualifying instance %s: spot" % (instance['InstanceId']))
+        if instance['State']['Name'] == "running" and not 'SpotInstanceRequestId' in instance:
+            az = instance['Placement']['AvailabilityZone']
+            if 'VpcId' in instance and options.vpc_sensitive:
+                az = az + '_vpc'
+            if 'Platform' in instance:
+                az = az + '_windows'
+            instance_type = instance['InstanceType']
+            running_instances[(instance_type, az)] = running_instances.get((instance_type, az), 0) + 1
+
+if ignore and ignore['running']:
+    for ignore_item in ignore['running']:
+        if (ignore_item['type'], ignore_item['zone']) in running_instances:
+            running_instances[(ignore_item['type'], ignore_item['zone'])] = \
+                running_instances[(ignore_item['type'], ignore_item['zone'])] - ignore_item['count']
+
+reserved_instances = {}
+reserved_list = ec2_conn.describe_reserved_instances()
+for reserved_instance in reserved_list['ReservedInstances']:
+    if debug and reserved_instance['State'] != "active":
+        print("Excluding reserved instances %s: no longer active" % (reserved_instance['ReservedInstancesId']))
+    if reserved_instance['State'] == "active":
+        az = reserved_instance['AvailabilityZone']
+        if 'VPC' in reserved_instance['ProductDescription'] and options.vpc_sensitive:
+            az = az + '_vpc'
+        if 'Windows' in reserved_instance['ProductDescription']:
+            az = az + '_windows'
+        instance_type = reserved_instance['InstanceType']
+        reserved_instances[(instance_type, az)] = reserved_instances.get((instance_type, az), 0) + \
+                                                  reserved_instance['InstanceCount']
+
+if ignore and ignore['reserved']:
+    for ignore_item in ignore['reserved']:
+        if (ignore_item['type'], ignore_item['zone']) in reserved_instances:
+            reserved_instances[(ignore_item['type'], ignore_item['zone'])] = \
+                reserved_instances[(ignore_item['type'], ignore_item['zone'])] - ignore_item['count']
+
+# this dict will have a positive number if there are unused reservations
+# and negative number if an instance is on demand
+instance_diff = dict([(x, reserved_instances[x] - running_instances.get(x, 0)) for x in reserved_instances])
+
+# instance_diff only has the keys that were present in reserved_instances. There's probably a cooler way to add a filtered dict here
+for placement_key in running_instances:
+    if not placement_key in reserved_instances:
+        instance_diff[placement_key] = -running_instances[placement_key]
+
+unused_reservations = dict((key, value) for key, value in instance_diff.iteritems() if value > 0)
+if unused_reservations == {}:
+    print("Congratulations, you have no unused reservations")
+else:
+    for unused_reservation in unused_reservations:
+        print("UNUSED RESERVATION!\t(%s)\t%s\t%s" % (
+            unused_reservations[unused_reservation], unused_reservation[0], unused_reservation[1]))
+
+print("")
+
+unreserved_instances = dict((key, -value) for key, value in instance_diff.iteritems() if value < 0)
+if unreserved_instances == {}:
+    print("Congratulations, you have no unreserved instances")
+else:
+    for unreserved_instance in unreserved_instances:
+        print("Instance not reserved:\t(%s)\t%s\t%s" % (
+            unreserved_instances[unreserved_instance], unreserved_instance[0], unreserved_instance[1]))
+
+qty_running_instances = reduce(lambda x, y: x + y, running_instances.values())
+qty_reserved_instances = reduce(lambda x, y: x + y, reserved_instances.values())
+
+print("\n(%s) running on-demand instances\n(%s) reservations" % (qty_running_instances, qty_reserved_instances))
